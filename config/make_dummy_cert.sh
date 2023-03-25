677bd751d75813f2a111c33f0ef1acd562e9a314
Matthew Morgan <matthew.morgan@stelligent.com>
2023-03-24 17:36:52 -0700

    refactor dummy cert

--- config/make_dummy_cert.sh
+++ config/make_dummy_cert.sh
@@ -11,19 +11,19 @@ answers() {
         echo hexfury@hotmail.com
 }
 
-if [ $# -eq 0 ] ; then
-        echo $'Usage: (basename $0) filename [...]'
+if [ $# -lt 2 ] ; then
+        echo "Usage: (basename $0) filename1 filename2 filename3"
+        echo "Usage: filename1 ~= server.crt"
+        echo "Usage: filename2 ~= server.key"
+        echo "Usage: filename3 ~= dhparams.pem"
         exit 0
 fi
 
-for target in $@ ; do
-        PEM1=$(/bin/mktemp /tmp/openssl.XXXXXX)
-        PEM2=$(/bin/mktemp /tmp/openssl.XXXXXX)
-        trap 'rm -f ${PEM1} ${PEM2}' INT
-        answers | /usr/bin/openssl req -newkey rsa:2048 -keyout "${PEM1}" -nodes -x509 -days 365 -out "${PEM2}" 2> /dev/null
-        /usr/bin/openssl dhparam -out "${target}/dhparam.pem" 4096
-        cat "${PEM1}" >  "${target}"
-        echo ''   >> "${target}"
-        cat "${PEM2}" >> "${target}"
-        rm -f "${PEM1}" "${PEM2}"
-done
+PEM1=$(/bin/mktemp /tmp/openssl.XXXXXX)
+PEM2=$(/bin/mktemp /tmp/openssl.XXXXXX)
+trap 'rm -f ${PEM1} ${PEM2}' INT
+answers | /usr/bin/openssl req -newkey rsa:2048 -keyout "${PEM1}" -nodes -x509 -days 365 -out "${PEM2}" 2> /dev/null
+cat "${PEM1}" > "${1}"
+cat "${PEM2}" >> "${2}"
+rm -f "${PEM1}" "${PEM2}"
+/usr/bin/openssl dhparam -out "${3}" 4096
